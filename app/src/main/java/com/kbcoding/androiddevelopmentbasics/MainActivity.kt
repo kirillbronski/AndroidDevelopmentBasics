33013fe1e816a37093d3c3ced92a53d105dda718
Kirill <kirillbronski@gmail.com>
2023-08-25 17:25:31 +0300

    alert dialog examples

--- app/src/main/java/com/kbcoding/androiddevelopmentbasics/MainActivity.kt
+++ app/src/main/java/com/kbcoding/androiddevelopmentbasics/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.kbcoding.androiddevelopmentbasics
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kbcoding.androiddevelopmentbasics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.level1Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel1Activity::class.java))
+        }
+
+        binding.level2Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel2Activity::class.java))
+        }
+
+        binding.exitButton.setOnClickListener {
+            finish()
+        }
     }
 }
