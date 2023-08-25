797a83f2a300940b9f98c5c6109f54248d1610a9
Kirill <kirillbronski@gmail.com>
2023-08-25 15:44:07 +0300

    implemented main activity

--- app/src/main/java/com/kbcoding/androiddevelopmentbasics/MainActivity.kt
+++ app/src/main/java/com/kbcoding/androiddevelopmentbasics/MainActivity.kt
@@ -2,10 +2,68 @@ package com.kbcoding.androiddevelopmentbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentManager.FragmentLifecycleCallbacks
+import com.kbcoding.androiddevelopmentbasics.databinding.ActivityMainBinding
+import java.util.UUID
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity(), Navigator {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val fragmentListener = object : FragmentLifecycleCallbacks() {
+        override fun onFragmentViewCreated(
+            fm: FragmentManager,
+            f: Fragment,
+            v: View,
+            savedInstanceState: Bundle?
+        ) {
+            super.onFragmentViewCreated(fm, f, v, savedInstanceState)
+            update()
+        }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportFragmentManager.registerFragmentLifecycleCallbacks(fragmentListener, false)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainer, createFragment())
+                .commit()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        supportFragmentManager.unregisterFragmentLifecycleCallbacks(fragmentListener)
+    }
+
+    override fun launchNext() {
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .add(R.id.fragmentContainer, createFragment())
+            .commit()
+    }
+
+    override fun generateUuid(): String = UUID.randomUUID().toString()
+
+    override fun update() {
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        if (currentFragment is HasUuid) {
+            binding.currentFragmentUuidTextView.text = currentFragment.getUuid()
+        } else {
+            binding.currentFragmentUuidTextView.text = ""
+        }
+        if (currentFragment is NumberListener) {
+            currentFragment.onNewScreenNumber(1 + supportFragmentManager.backStackEntryCount)
+        }
+    }
+
+    private fun createFragment(): RandomFragment {
+        return RandomFragment.newInstance(generateUuid())
+    }
+
 }
