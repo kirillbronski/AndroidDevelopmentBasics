c8c5b98ef99f02d52266dfd54e7a5175524d51b3
Kirill <kirillbronski@gmail.com>
2023-09-09 21:17:02 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation ("androidx.fragment:fragment-ktx:1.6.1")
     implementation("com.github.javafaker:javafaker:1.0.2")
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
