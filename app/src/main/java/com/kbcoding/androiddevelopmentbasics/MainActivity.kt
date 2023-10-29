4211e0640cb9f33ce37f3734e1d180c31ba560d4
Kirill <kirillbronski@gmail.com>
2023-10-29 20:09:25 +0300

    35 permissions

--- app/src/main/java/com/kbcoding/androiddevelopmentbasics/MainActivity.kt
+++ app/src/main/java/com/kbcoding/androiddevelopmentbasics/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.kbcoding.androiddevelopmentbasics
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.kbcoding.androiddevelopmentbasics.databinding.ActivityMainBinding
 import com.kbcoding.androiddevelopmentbasics.presentation.currentColor.CurrentColorFragment
 import com.kbcoding.core.presentation.activity.BaseActivity
 import com.kbcoding.core.sideEffects.SideEffectPluginsManager
@@ -12,34 +22,85 @@ import com.kbcoding.core.sideEffects.permissions.plugin.PermissionsPlugin
 import com.kbcoding.core.sideEffects.resources.plugin.ResourcesPlugin
 import com.kbcoding.core.sideEffects.toasts.plugin.ToastsPlugin
 
-class MainActivity : BaseActivity() {
+/**
+ * Example of requesting runtime permission with Activity Result API.
+ */
+class MainActivity : AppCompatActivity() {
 
-    override fun registerPlugins(manager: SideEffectPluginsManager) = with (manager) {
-        val navigator = createNavigator()
-        register(ToastsPlugin())
-        register(ResourcesPlugin())
-        register(NavigatorPlugin(navigator))
-        register(PermissionsPlugin())
-        register(DialogsPlugin())
-        register(IntentsPlugin())
-    }
+    // you may use only 1 launcher if the response handling logic is the same
+    // for all permissions
+
+    private val requestCameraPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),    // contract for requesting 1 permission
+        ::onGotCameraPermissionResult
+    )
+
+    private val requestRecordAudioAndLocationPermissionsLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions(),   // contract for requesting more than 1 permission
+        ::onGotRecordAudioAndLocationPermissionsResult
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Initializer.initDependencies()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.requestCameraPermissionButton.setOnClickListener {
+            requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+
+        binding.requestRecordAudioAndLocationPermissionsButton.setOnClickListener {
+            requestRecordAudioAndLocationPermissionsLauncher.launch(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO)
+            )
+        }
+
     }
 
-    private fun createNavigator() = StackFragmentNavigator(
-        containerId = R.id.fc_main_container,
-        defaultTitle = getString(R.string.app_name),
-        animations = StackFragmentNavigator.Animations(
-            enterAnim = R.anim.enter,
-            exitAnim = R.anim.exit,
-            popEnterAnim = R.anim.pop_enter,
-            popExitAnim = R.anim.pop_exit
-        ),
-        initialScreenCreator = { CurrentColorFragment.Screen() }
-    )
+    private fun onGotCameraPermissionResult(granted: Boolean) {
+        if (granted) {
+            onCameraPermissionGranted()
+        } else {
+            // example of handling 'Deny & don't ask again' user choice
+            if (!shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                askUserForOpeningAppSettings()
+            } else {
+                Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun onGotRecordAudioAndLocationPermissionsResult(grantResults: Map<String, Boolean>) {
+        if (grantResults.entries.all { it.value }) {
+            onRecordAudioAndLocationPermissionsGranted()
+        }
+    }
+
+    private fun askUserForOpeningAppSettings() {
+        val appSettingsIntent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", packageName, null)
+        )
+        if (packageManager.resolveActivity(appSettingsIntent, PackageManager.MATCH_DEFAULT_ONLY) == null) {
+            Toast.makeText(this, R.string.permissions_denied_forever, Toast.LENGTH_SHORT).show()
+        } else {
+            AlertDialog.Builder(this)
+                .setTitle(R.string.permission_denied)
+                .setMessage(R.string.permission_denied_forever_message)
+                .setPositiveButton(R.string.open) { _, _ ->
+                    startActivity(appSettingsIntent)
+                }
+                .create()
+                .show()
+        }
+    }
+
+    private fun onRecordAudioAndLocationPermissionsGranted() {
+        Toast.makeText(this, R.string.audio_and_location_permissions_granted, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onCameraPermissionGranted() {
+        Toast.makeText(this, R.string.camera_permission_granted, Toast.LENGTH_SHORT).show()
+    }
 
 }
