4dcf2a7d660a7f714374aa8517d8f58d5af60e5d
Kirill <kirillbronski@gmail.com>
2023-11-25 18:25:35 +0300

    41 implemented AccountDbEntity

---
+++ app/src/main/java/com/kbcoding/androiddevelopmentbasics/data/room/entities/AccountDbEntity.kt
@@ -0,0 +1,40 @@
+package com.kbcoding.androiddevelopmentbasics.data.room.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.kbcoding.androiddevelopmentbasics.domain.model.accounts.Account
+import com.kbcoding.androiddevelopmentbasics.domain.model.accounts.SignUpData
+
+@Entity(
+    tableName = "accounts",
+    indices = [
+        Index("email", unique = true)
+    ]
+)
+data class AccountDbEntity(
+    @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) val id: Long,
+    @ColumnInfo(name = "email", collate = ColumnInfo.NOCASE) val email: String,
+    @ColumnInfo(name = "username") val username: String,
+    @ColumnInfo(name = "password") val password: String,
+    @ColumnInfo(name = "created_at") val createdAt: Long
+) {
+
+    fun toAccount(): Account = Account(
+        id = id,
+        email = email,
+        username = username,
+        createdAt = createdAt
+    )
+
+    companion object {
+        fun fromSignUpData(signUpData: SignUpData) = AccountDbEntity(
+            id = 0, // SQLite generates identifier automatically if ID = 0
+            email = signUpData.email,
+            username = signUpData.username,
+            password = signUpData.password,
+            createdAt = System.currentTimeMillis()
+        )
+    }
+}
