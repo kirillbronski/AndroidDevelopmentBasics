0fe78f013314e683761b2b8b468ecfafde10add0
Kirill <kirillbronski@gmail.com>
2023-12-21 22:20:15 +0300

    43 update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,25 +51,25 @@ android {
 dependencies {
 
     // Room
-    implementation("androidx.room:room-runtime:2.6.0")
+    implementation("androidx.room:room-runtime:2.6.1")
     //kapt("androidx.room:room-compiler:2.6.0")
-    ksp("androidx.room:room-compiler:2.6.0")
-    implementation("androidx.room:room-ktx:2.6.0")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 
     implementation ("androidx.fragment:fragment-ktx:1.6.2")
-    implementation ("androidx.activity:activity-ktx:1.8.1")
+    implementation ("androidx.activity:activity-ktx:1.8.2")
     implementation("com.github.javafaker:javafaker:1.0.2")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
