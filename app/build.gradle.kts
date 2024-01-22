a6244290555ca1d2b128e5c562412e5659177dd1
Kirill <kirillbronski@gmail.com>
2024-01-22 12:51:07 +0300

    59 add packaging block to build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.api.dsl.Packaging
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -29,6 +31,11 @@ android {
                 "proguard-rules.pro"
             )
         }
+    }
+
+    packaging {
+        resources.excludes.add("META-INF/LICENSE.md")
+        resources.excludes.add("META-INF/LICENSE-notice.md")
     }
 
     ksp {
@@ -86,8 +93,8 @@ dependencies {
     implementation("io.coil-kt:coil:2.2.0")
     implementation("com.elveum:element-adapter:0.3.1")
 
-    implementation ("androidx.fragment:fragment-ktx:1.6.2")
-    implementation ("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -104,16 +111,16 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.2")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
 
-    androidTestImplementation ("org.hamcrest:hamcrest:2.2")
-    androidTestImplementation ("androidx.test:runner:1.5.2")
-    androidTestImplementation ("androidx.test:rules:1.5.0")
-    androidTestImplementation ("androidx.test.espresso:espresso-contrib:3.5.1")
-    androidTestImplementation ("androidx.test.espresso:espresso-intents:3.5.1")
-    androidTestImplementation ("androidx.arch.core:core-testing:2.2.0")
+    androidTestImplementation("org.hamcrest:hamcrest:2.2")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.2")
     androidTestImplementation("androidx.navigation:navigation-testing:2.7.6")
     androidTestImplementation("com.google.dagger:hilt-android-testing:2.50")
     kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.50")
-    //debugImplementation("io.mockk:mockk-android:1.13.9")
+    debugImplementation("io.mockk:mockk-android:1.13.9")
     debugImplementation("androidx.fragment:fragment-testing:1.6.2")
 }
